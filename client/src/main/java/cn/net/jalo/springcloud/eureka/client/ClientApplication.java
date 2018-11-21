4461ad34db54ecb1c23926d3b0003558767f30d9
Jalo <651379728@qq.com>
2018-11-21 18:00:41 +0800

    Subproject add Requestmapping '/' to print 'This is a client.'

--- client/src/main/java/cn/net/jalo/springcloud/eureka/client/ClientApplication.java
+++ client/src/main/java/cn/net/jalo/springcloud/eureka/client/ClientApplication.java
@@ -3,12 +3,21 @@ package cn.net.jalo.springcloud.eureka.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class ClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public @ResponseBody String testPrint() {
+		return "This is a client.";
 	}
 }
